bf3a7778a257cc050874eae0c886bf3b5f3af7a1
Cyro Dubeux <xorycx@gmail.com>
2019-02-06 20:26:41 -0300

    Version

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ const About = () => (
     <Container>
         <View style={styles.content}>
             <Title style={styles.text}>
-                Feel the Movies v4.1.13
+                Feel the Movies v4.1.14
             </Title>
 
             <Text style={styles.body}>
